e831d57bcf84c76fac1250c50696cb7fb9e8f708
Vimalkumar <j.vimal@gmail.com>
2011-05-01 19:54:27 -0700

    streamlined app and made it userfriendly
    no need to start mongodb separately.  you can now invoke it
    within this python program and just point it to the source
    directory.

--- instant.py
+++ instant.py
@@ -8,6 +8,12 @@ import sys
 import json
 from flask import Flask,url_for,redirect
 
+import argparse
+from subprocess import Popen, PIPE
+import os
+from time import sleep
+import termcolor as T
+
 app = Flask(__name__)
 connection = None
 db = None
@@ -15,23 +21,108 @@ t = None
 dbname = 'test'
 tagscollection = 'tags'
 started = False
+mongod_process = None
+
+parser = argparse.ArgumentParser(description="ctags instant")
+parser.add_argument('--source-dir',
+                    '-d',
+                    action='store',
+                    dest='source_dir',
+                    help="Path to the source code's root directory.",
+                    required=True)
+
+parser.add_argument('--mongod',
+                    '-e',
+                    action='store',
+                    dest='mongod_path',
+                    help="Path to the mongod executable.",
+                    default='/usr/local/bin/mongod')
+
+parser.add_argument('--quiet',
+                    '-q',
+                    action='store_true',
+                    dest='quiet',
+                    help="Suppress mongod's output.",
+                    default=False)
+
+args = None
 
 def log(s):
-    print s
+    print T.colored(s, "yellow")
+
+def check_mongod():
+    args.mongod_path = os.path.expanduser(args.mongod_path)
+    if os.path.exists(args.mongod_path):
+        return True
+    retry_path = check_output(["which", "mongod"]).strip()
+    if len(retry_path):
+        log("Found mongod elsewhere: %s" % retry_path)
+        args.mongod_path = retry_path
+        return True
+    return False
+
+def check_db(root_dir):
+    db_dir = os.path.join(root_dir, "mongo_db")
+    db_dir = os.path.expanduser(db_dir)
+    return os.path.exists(db_dir)
+
+def create_db(root_dir):
+    try:
+        db_dir = os.path.join(root_dir, "mongo_db")
+        db_dir = os.path.expanduser(db_dir)
+        os.mkdir(db_dir)
+    except Exception, e:
+        log("Couldn't make directory %s.  Check permissions" % db_dir)
+        print e
+        sys.exit(-1)
+
+def start_mongod(dbpath='db/'):
+    if not check_mongod():
+        log("Error: mongod not found.")
+        sys.exit(-1)
+    if not check_db(dbpath):
+        log("Database directory not found.  creating...")
+        create_db(dbpath)
+    cmd = "%s --dbpath=\"%s\"" % (args.mongod_path, dbpath)
+    if args.quiet:
+        ret = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    else:
+        ret = Popen(cmd, shell=True)
+    return ret
 
 def start():
     global connection, db, dbname
     global tagscollection, t, started
+    global mongod_process
+
     if started:
         return
-    connection = Connection()
+    args.source_dir = os.path.expanduser(args.source_dir)
+    # Start mongodb
+    dbpath = os.path.join(args.source_dir, "mongo_db")
+    mongod_process = start_mongod(dbpath=dbpath)
+    while 1:
+        try:
+            log("trying to connect to mongod...")
+            connection = Connection()
+            break
+        except:
+            sleep(1)
+            continue
+    dbname = args.source_dir.replace('/','|')
     db = connection[dbname]
     t = db[tagscollection]
+    log("Found databases: %s" % (', '.join(connection.database_names())))
+    if dbname not in connection.database_names():
+        log("Database for this project does not exist.  Creating one...")
+        create_project(args.source_dir)
     started = True
 
 def stop():
     started = False
-    pass
+    mongod_process.terminate()
+    log("Waiting for mongod to terminate...")
+    mongod_process.wait()
 
 def read_ctags(fname):
     lines = open(fname).readlines()
@@ -54,19 +145,26 @@ def read_ctags(fname):
     return lst
 
 def populate_db(fname):
-    start()
     log('Reading tags')
     tags = read_ctags(fname)
     t.insert(tags)
     log('Populated db')
 
+def create_project(source_dir):
+    # First, we create the TAGS file for this project
+    tags_file = os.path.join(source_dir, "tags_mongo")
+    log("Creating tags... %s, %s" % ( tags_file, source_dir))
+    Popen("ctags -R --fields=+afmikKlnsStz -f \"%s\" \"%s\"" % (tags_file, source_dir),
+          shell=True).wait()
+    log("Reading tags...")
+    populate_db(tags_file)
+    index()
+
 def drop_db():
-    start()
     db.drop_collection(tagscollection)
 
 def index():
     """Sets up an index for the particular column."""
-    start()
     log("Creating indices..")
     t.create_index([
             ("token", pymongo.ASCENDING),
@@ -75,7 +173,6 @@ def index():
     log("Done creating indices..")
 
 def db_search(limit=20,**kwargs):
-    start()
     dic = {}
     for k,v in kwargs.iteritems():
         dic[k] = {'$regex': '%s' % v}
@@ -142,5 +239,11 @@ def main():
     return redirect('/static/index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
-
+    try:
+        args = parser.parse_args()
+        start()
+        app.run(host='0.0.0.0', debug=True)
+    except Exception, e:
+        log("Error...")
+        log(`e`)
+        mongod_process.terminate()
